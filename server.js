2d29fc6a1c7ecb59261623cd6bf60d580625411d
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-08-29 13:46:12 +0100

    routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const connectDB = require('./config/db')
+const express = require('express');
+const connectDB = require('./config/db');
 
-app.get('/', (req, res) => res.send('API Running'))
+const app = express();
 
-connectDB()
+connectDB();
 
-const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('API Running'));
 
-app.listen(PORT, () => console.log(`The server started on port ${PORT}`))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server started on port ${PORT}`));
