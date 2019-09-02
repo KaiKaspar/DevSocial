99e1bc7cd19330cbb264f5428612a1d5191d2339
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-09-02 13:45:46 +0100

    Get profiles and other users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator/check')
 
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -16,6 +17,103 @@ router.get('/me', auth, async (req, res) => {
         res.json(profile)
     } catch(err) {
         console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+router.post('/', [ auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubUsername,
+        skills,
+        youtube,
+        facebook,
+        instagram,
+        twitter,
+        linkedin
+    } = req.body
+
+    // build profile object
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if(company) profileFields.company = company
+    if(website) profileFields.website = website
+    if(location) profileFields.location = location
+    if(bio) profileFields.bio = bio
+    if(status) profileFields.status = status
+    if(githubUsername) profileFields.githubUsername = githubUsername
+    if(skills){
+        profileFields.skills = skills.split(',').map(skill => skill.trim())
+    }
+
+    // build social object
+    profileFields.social = {}
+    if(youtube) profileFields.social.youtube = youtube
+    if(twitter) profileFields.social.twitter = twitter
+    if(facebook) profileFields.social.facebook = facebook
+    if(linkedin) profileFields.social.linkedin = linkedin
+    if(instagram) profileFields.social.instagram = instagram
+
+    try {
+        let profile = await Profile.findOne({ user: req.user.id })
+
+        if(profile){
+            // update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            )
+
+            return res.json(profile)
+        }
+
+        // create
+        profile = new Profile(profileFields)
+        await profile.save()
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+
+        if(!profile) return res.status(400).json({ msg: "Profile not found"})
+
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message)
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: "Profile not found"})
+        }
         res.status(500).send('Server Error')
     }
 })
