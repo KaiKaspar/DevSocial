816c86986ee291ed169c7f3f5a39b5027b46332a
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-10-17 14:51:52 +0100

    display github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -37,6 +38,9 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                             ))}
                         </Fragment>) : (<h4>No education Credentials</h4>)}
                     </div>
+                    {profile.githubUsername && (
+                        <ProfileGithub username={profile.githubUsername}/>
+                    )}
                 </div>
             </Fragment>}
         </Fragment>
